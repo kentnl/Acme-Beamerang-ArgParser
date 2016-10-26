4fe1544593825db8e221ff4f3bab5d261989c1ac
Kent Fredric <kentfredric@gmail.com>
2016-10-27 03:42:19 +1300

    Flesh out documentation of existing features.

--- lib/Acme/Beamerang/ArgParser.pm
+++ lib/Acme/Beamerang/ArgParser.pm
@@ -117,6 +117,17 @@ Acme::Beamerang::ArgParser - A partial commandlet oriented args parser
   my $worker = My::Sub::Dispatcher::Thing->new( %$state );
   $worker->exec( @{ $cfg->extra_args } );
 
+=head1 EXPERIMENTAL
+
+This code is reasonably simple, but its really an experimental prototype for a
+bunch of things I'm working on, and is subject to change.
+
+Any finalized results of said project may use other parts, or be implemented
+differently.
+
+This module is subsequently prone to radical changes until a way forward ( and
+even a name ) is decided upon.
+
 =head1 DESCRIPTION
 
 C<B<Acme::Beamerang::ArgParser>> is an experimental argument parsing utility to
@@ -178,6 +189,112 @@ C<--dispatcher> and the command being C<legacy> (LOL!), or C<--size> not
 supporting arguments, and the command being C<10> (LOL!)
 
 And this design choice is considered an important axiom of this approach.
+
+=head1 METHODS
+
+=head2 C<new>
+
+  my $instance = Acme::Beamerang::ArgParser->new( filter => ['h','help','v','version' ]);
+
+Creates a new instance of an ArgParser
+
+=head3 C<new.filter>
+
+An C<ArrayRef> of arguments to filter and deem as "ours"
+
+=over 4
+
+=item * Single character arguments map to single-dashed extractors
+
+=item * Multi-Character arguments map to double-dashed extractors.
+
+=back
+
+=head2 C<parse>
+
+  my $cfg = $instance->parse( @ARGV );
+
+Processes input C<@ARGV> and filters its content into categories based on
+C<filter>, halting processing at the first token that I<doesn't> start with a
+C<->.
+
+Returns a C<::Cfg> instance describing the arguments.
+
+=head3 C<Cfg.args>
+
+Returns the arguments deemed "ours" as an C<ArrayRef> of decoded args.
+
+  for my $arg (@{ $cfg->args }) {
+    my ( $argname, $argvalue ) = @{ $arg };
+
+    do_help and last if $argname eq 'h' or $argname eq 'help';
+
+    if ( $argname eq 'output' or $argname eq 'o' ) {
+      die "output/o takes an argument" unless defined $argvalue and length $argvalue;
+      set_output( $argvalue ) and next;
+    }
+  }
+
+Each arg is an C<ArrayRef> as follows:
+
+  [ 'arg'          ] # --arg
+  [ 'arg', ''      ] # --arg=
+  [ 'arg', 'value' ] # --arg=value
+
+Where C<arg> can only be values passed in via C<filter>
+
+=head3 C<Cfg.command>
+
+  my $command = $cfg->command
+
+Contains the value of the first non-parameter argument, or C<undef> if none was
+visited during parse.
+
+=head3 C<Cfg.orig_args>
+
+Contains an C<ArrayRef> of the original unmodified arguments that were passed
+in.
+
+  my (@orig_args) = @{ ::ArgParser->new( filter => [ qw/ hello / ])->parse( @ARGV )->orig_args }
+
+This is mostly for debugging purposes.
+
+=head3 C<Cfg.unknown_args>
+
+Contains an C<ArrayRef> of all hyphen prefixed arguments that were seen prior
+to the L<C<command>|/Cfg.command> that were not defined in the filter list.
+
+  my (@unknown_args) = @{ $cfg->unknown_args };
+
+The presence of any items in this list can be used to cause validation errors,
+or they can be assumed to be transparent arguments to pass down to the next
+level.
+
+=head3 C<Cfg.unparsed_args>
+
+Contains an C<ArrayRef> of all tokens that succeeded the C<command> ( excluding
+the command itself ).
+
+  my (@unparsed_args) = @{ $cfg->unparsed_args };
+
+These are to be considered "Property of the C<command>"
+
+=head3 C<Cfg.extra_args>
+
+Returns an C<ArrayRef> equivalent to the input arguments to C<parse>, minus the
+contents of L<C<.args>|/Cfg.args>.
+
+  my (@extra_args) = @{ $cfg->extra_args };
+
+=head1 ENVIRONMENT
+
+This module consumes L<< C<Acme::Beamerang::Logger>|Acme::Beamerang::Logger >>
+and subsequently will respond to all respective flags it supports.
+
+      BEAMERANG_ARGPARSER_$LEVEL
+      BEAMERANG_$LEVEL
+      BEAMERANG_ARGPARSER_UPTO
+      BEAMERANG_UPTO
 
 =head1 AUTHOR
 
